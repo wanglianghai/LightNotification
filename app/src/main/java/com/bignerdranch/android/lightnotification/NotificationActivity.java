e8e429e04067d1793cf8c871eea9056e705fa512
wanglianghai <1540446221@qq.com>
2017-08-11 14:00:07 +0800

    折叠通知，自定义通知

--- app/src/main/java/com/bignerdranch/android/lightnotification/NotificationActivity.java
+++ app/src/main/java/com/bignerdranch/android/lightnotification/NotificationActivity.java
@@ -4,10 +4,12 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RemoteViews;
 
 public class NotificationActivity extends AppCompatActivity {
     private NotificationManager mManager;
@@ -27,9 +29,25 @@ public class NotificationActivity extends AppCompatActivity {
         findViewById(R.id.folder_notification).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                custom();
             }
         });
+    }
+
+    private void custom() {
+        RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.folder_view);
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.baidu.com"));
+        PendingIntent pendingIntent = PendingIntent.getActivity(NotificationActivity.this, 0, intent, 0);
+        Notification notification = new Notification.Builder(NotificationActivity.this)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+                .setSmallIcon(R.drawable.launcher)
+                .setContentTitle("折叠式通知")
+                .setContentText("default")
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                .build();
+        notification.bigContentView = remoteViews;
+        mManager.notify(1, notification);
     }
 
     private void normalNotification() {
